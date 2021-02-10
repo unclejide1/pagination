4b4c65cc77dcc2f0320b9a6310d5e622141cc3b2
Jideofor <njusticej@gmail.com>
2021-02-10 23:04:57 +0100

    feat: display buttons dynamically to change pages

--- src/App.js
+++ src/App.js
@@ -5,26 +5,36 @@ function App() {
   const {loading, data} = useFetch()
   const [page, setPage] = useState(0)
   const [followers, setFollowers] = useState ([])
+  console.log(data.length)
 
   useEffect(() => {
     if(loading) return 
     setFollowers(data[page])
-  }, [loading])
+  }, [loading, page, data])
   console.log(data)
-  return <main>
-    <div className="section-title">
-      <h1>{loading ? 'loading...' : 'pagination'}</h1>
-      <div className="underline"></div>
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
 
-      <section className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <Follower key={follower.id} {...follower}/>
-          })}
-        </div>
-      </section>
-    </div>
-  </main>
+        <section className='followers'>
+          <div className='container'>
+            {followers.map((follower) => {
+              return <Follower key={follower.id} {...follower} />
+            })}
+          </div>
+          {!loading && <div className='btn-container'>
+            {data.map((item, index) => {
+                return <button key={index} className={`page-btn ${index===page? 'active-btn': null}`} onClick={() => setPage(index)}>
+                  {index + 1}
+                </button>
+            })}
+            </div>}
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App
