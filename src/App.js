1539a42fbeccd8eb8ef82c1bb35e044bc352d825
Jideofor <njusticej@gmail.com>
2021-02-10 22:41:08 +0100

    feat: display first page on initial render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
   const {loading, data} = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState ([])
+
+  useEffect(() => {
+    if(loading) return 
+    setFollowers(data[page])
+  }, [loading])
   console.log(data)
   return <main>
     <div className="section-title">
@@ -11,7 +18,7 @@ function App() {
 
       <section className="followers">
         <div className="container">
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower}/>
           })}
         </div>
